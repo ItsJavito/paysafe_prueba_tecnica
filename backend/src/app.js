ad2987f847d8a95c0479131ba5cc7114c587b450
Javier <javierenriqueos@gmail.com>
2024-06-21 19:51:32 -0500

    configuraciones iniciales del backend

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// conexion a base de datos
+
+
+// configuracion de express
+const app = express();
+
+//middlewares 
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+
+module.exports = app; 
