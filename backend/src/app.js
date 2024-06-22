00960e38b909a551a6df66080959b99b30c82374
Javier <javierenriqueos@gmail.com>
2024-06-21 20:23:21 -0500

    add middleware and dbconnect

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const connectDB = require('./config/db');
 
 // conexion a base de datos
-
+connectDB();
 
 // configuracion de express
 const app = express();
