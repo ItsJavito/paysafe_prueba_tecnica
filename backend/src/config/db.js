1007899b6462c0e7eae08bdf2e1a9c8052d4e888
Javier <javierenriqueos@gmail.com>
2024-06-21 20:51:40 -0500

    config db connect

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed', error);
         process.exit(1);
